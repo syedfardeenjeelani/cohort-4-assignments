567ec4b1ff46e8f72c34355fa7f594416b7bf991
syedfardeenjeelani <syedfardeenjeelani13@gmail.com>
2026-01-17 19:38:11 +0530

    feat: converted this to vite

---
+++ elevanLabs/src/main.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+
+const scene = new THREE.Scene();
+
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+function animate() {
+  renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
