e18bd6c44c3129895a0bacf1c634e0fa05c00b60
syedfardeenjeelani <syedfardeenjeelani13@gmail.com>
2026-01-18 00:12:50 +0530

    box

--- elevanLabs/src/main.js
+++ elevanLabs/src/main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const renderer = new THREE.WebGLRenderer();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 85, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 
@@ -14,12 +14,14 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.getElementById("leftParent").appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: `#E9E9E9` } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
 function animate() {
+  cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
   renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
